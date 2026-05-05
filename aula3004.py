51533cbf64fb7fa14847ec6ea159bb080f69793a
27anajulia27-jpg <27anajulia27@gmail.com>
2026-05-05 15:20:14 -0300

    Add files via upload

---
+++ aula3004.py
@@ -0,0 +1,13 @@
+lista = ["kamilly" , "Lohany", "Isabella","Emanuel"]
+
+nome1 = input("Digite o primeiro nome:")
+nome2 = input("Digite o segundo nome:")
+nome3 = input("Digite o terceiro o nome:")
+nome4 = input("Digite o quarto nome:")
+
+lista.append(nome1)
+lista.append(nome2)
+lista.append(nome3)
+lista.append(nome4)
+
+print(lista)
